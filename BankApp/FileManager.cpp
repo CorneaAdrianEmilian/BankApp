4b38147e2d38575e12c49f09f527597e01e162ea
Cornea Adrian Emilian <emilian.cornea@yahoo.com>
2021-10-01 18:15:55 +0300

    merge2

--- BankApp/FileManager.cpp
+++ BankApp/FileManager.cpp
@@ -1,3 +1,4 @@
+
 #include "FileManager.h"
 std::shared_ptr<FileManager> FileManager::instanta = nullptr;
 FileManager::FileManager()
@@ -60,4 +61,4 @@ std::shared_ptr<FileManager> FileManager::getInstanta()
 		instanta = std::make_unique<FileManager>();
 		return instanta;
 	}
-}
+
